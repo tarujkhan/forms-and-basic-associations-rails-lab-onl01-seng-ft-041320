3e36410ec538a438d522dc45b2b4c9fced798d75
taruj khan <tarujkhan@DESKTOP-E8JDKB5.localdomain>
2020-06-19 15:54:13 -0500

    This lab is done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,10 @@
 class Genre < ActiveRecord::Base
+  has_many :songs 
   # add associations
+  def genre_id=(id)
+    id.each do |i|
+      genre = Genre.find(i)
+      self.genres << genre
+    end
+  end
 end
